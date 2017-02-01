f88bb1d3de5625dcc0a52902460860df73c72d7d
Thomas Grauer <tgrauer1@me.com>
2017-02-01 08:11:53 -0500

    Route scroll fix

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
         </div>
     </div>
 
-    <div ng-view></div>
+    <div ng-view autoscroll="true"></div>
     
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
